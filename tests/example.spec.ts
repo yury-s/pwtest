a522cbcd9b25dfd7b83c2f058f2a9cd276ea41bf
Yury Semikhatsky <yurys@chromium.org>
2024-01-25 10:46:46 -0800

    toBlob jpeg

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,24 +1,23 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
-test.use({
-  headless: false,
-  userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
-});
+test.describe('Canvas to blob result type check', async () => {
+  test('Canvas to blob result type check @screen-all', async ({ page }) => {
+    const imageType = await page.evaluate(async () => {
+      const canvas = document.createElement('canvas');
 
-test("test", async ({page}) => {;
-  await page.route('**/*', async route => {
-    route.continue();
-    const headers = await route.request().allHeaders();
-    console.log(headers);
+      canvas.width = 100;
+      canvas.height = 100;
+
+      const getImageType = () =>
+        new Promise((resolve) => {
+          canvas.toBlob((ret) => {
+            resolve(ret?.type);
+          }, 'image/jpeg');
+        });
+
+      return await getImageType();
+    });
+
+    expect(imageType).toEqual('image/jpeg');
   });
-  const reqPromise = page.waitForRequest('https://www.bing.com/');
-  await page.goto('https://www.bing.com/');
-  const req = await reqPromise;
-  const headers = await req.allHeaders();
-  // console.log(headers);
-
-  const respHeaders = await (await req.response())!.allHeaders();
-  console.log(respHeaders);
-
-  // await new Promise(() => {});
 });
