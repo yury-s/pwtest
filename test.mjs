7f750c0522e76ce7bac8e2a4af61ccbb68693f8e
Yury Semikhatsky <yurys@chromium.org>
2024-06-03 15:45:22 -0700

    Add more files;

--- test.mjs
+++ test.mjs
@@ -1,14 +1,20 @@
-import { chromium } from "@playwright/test";
+import { webkit } from '@playwright/test';
 
-const context = await chromium.launchPersistentContext('/tmp/playwright', {headless: false});
-const page = await context.newPage();
-
-await page.goto("https://playwright.dev");
-
-
-await new Promise(f => setTimeout(f, 10000));
-
-console.log('pages: ', context.pages().length);
-
-await context.close();
-
+(async () => {
+  // Setup
+  const browser = await webkit.launch({ headless: false });
+  const context = await browser.newContext();
+  await context.tracing.start({ snapshots: true })
+  const page = await context.newPage();
+  try {
+    await page.goto('https://development-webapp-common-flow-auth.eks.csp.scrum-dev.com/login?role=customer');
+    await page.getByTestId('customer-login-email').fill('yen.vo+5@c2c-techhub.io');
+    await page.getByTestId('customer-login-password').fill('Aa123456@');
+    await page.getByTestId('customer-login-button').click()
+    await page.waitForTimeout(5000)
+  } finally {
+    await context.tracing.stop({ path: 'trace.zip' })
+  }
+  await context.close();
+  await browser.close();
+})();
