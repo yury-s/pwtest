18f8440a52500696ecf174d7f13f27a993690284
Yury Semikhatsky <yurys@chromium.org>
2024-06-06 16:45:09 -0700

    1.44.0

--- test.mjs
+++ test.mjs
@@ -1,20 +1,13 @@
-import { webkit } from '@playwright/test';
+import { chromium, expect } from '@playwright/test';
 
 (async () => {
   // Setup
-  const browser = await webkit.launch({ headless: false });
-  const context = await browser.newContext();
-  await context.tracing.start({ snapshots: true })
+  const browser = await chromium.launch({ headless: false });
+  const context = await browser.newContext({ ignoreHTTPSErrors: true, userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36","sec-ch-ua":"\"Chromium\";v=\"125\", \"Not.A/Brand\";v=\"24\"" });
   const page = await context.newPage();
-  try {
-    await page.goto('https://development-webapp-common-flow-auth.eks.csp.scrum-dev.com/login?role=customer');
-    await page.getByTestId('customer-login-email').fill('yen.vo+5@c2c-techhub.io');
-    await page.getByTestId('customer-login-password').fill('Aa123456@');
-    await page.getByTestId('customer-login-button').click()
-    await page.waitForTimeout(5000)
-  } finally {
-    await context.tracing.stop({ path: 'trace.zip' })
-  }
+  const url = "https://www.eet-china.com/mp/a319436.html";
+  const reponse = await page.goto(url, { referer: url, waitUntil: 'load' });
+  expect(reponse.status()).toBe(200);
   await context.close();
   await browser.close();
 })();
